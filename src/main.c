a85435288836d48b3d31bd43a3c6535c67f8f25d
fatima <fatima.dehbashii@gmail.com>
2024-01-02 22:58:28 +0330

    saving all file's information

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 #include <sys/mman.h>
 
 #define MAX_PATH_LENGTH 256
-#define MAX_PROCESSES 100
+#define MAX_FILES 1024
 
 // Structure to store file statistics
 typedef struct {
@@ -22,6 +22,7 @@ typedef struct {
 // Structure to store data (thread or process)
 struct data {
     char path[MAX_PATH_LENGTH];
+    FileInfo file_infos[MAX_FILES];
     FileInfo largest_file;
     FileInfo smallest_file;
     long total_files;       // Added member for total number of files
@@ -44,7 +45,9 @@ void calculateFileSize(FileInfo *fileInfo) {
 // Thread function to calculate file size
 void *calculateFileSizeThread(void *arg) {
     FileInfo *fileInfo = (struct FileInfo *)arg;
+    pthread_t tid = pthread_self();
     calculateFileSize(fileInfo);
+    printf("Thread %lu is analyzing file: %s\n", (unsigned long)tid, fileInfo->filename);
     return NULL;
 }
 void analyzeFile(const char *dirPath, struct data *shared_data) {
@@ -70,13 +73,20 @@ void analyzeFile(const char *dirPath, struct data *shared_data) {
         }
         // Construct the full path of the file
         snprintf(fileInfos[numFiles].filename, sizeof(fileInfos[numFiles].filename), "%s/%s", dirPath, entry->d_name);
-        // Create thread for each file to calculate file size
-        if (pthread_create(&threads[numFiles], NULL, calculateFileSizeThread, &fileInfos[numFiles]) != 0) {
-            perror("Error creating thread");
-            exit(EXIT_FAILURE);
+
+        // Check if the entry is a directory
+        if (entry->d_type == DT_DIR) {
+            // This is a subdirectory, analyze it recursively with threads
+            analyzeFile(fileInfos[numFiles].filename, shared_data);
+        } else {
+            // Create thread for each file to calculate file size
+            if (pthread_create(&threads[numFiles], NULL, calculateFileSizeThread, &fileInfos[numFiles]) != 0) {
+                perror("Error creating thread");
+                exit(EXIT_FAILURE);
+            }
+            //printf("Thread %d is analyzing file: %s\n", getpid(), fileInfos[numFiles].filename);
+            numFiles++;
         }
-        printf("Thread %d is analyzing directory: %s\n", getpid(), fileInfos[numFiles].filename);
-        numFiles++;
     }
     // Wait for all threads to finish
     for (int i = 0; i < numFiles; i++) {
@@ -86,22 +96,24 @@ void analyzeFile(const char *dirPath, struct data *shared_data) {
         }
     }
     for (int i = 0; i < numFiles; i++) {
+        pthread_mutex_lock(&shared_data->mutex);
+        if (shared_data->total_files < MAX_FILES) {
+            // Copy file info to the array
+            memcpy(&shared_data->file_infos[shared_data->total_files], &fileInfos[i], sizeof(FileInfo));
+        }
         shared_data->final_size += fileInfos[i].size;
         shared_data->total_files++;
         if (fileInfos[i].size > shared_data->largest_file.size) {
-            pthread_mutex_lock(&shared_data->mutex);
             shared_data->largest_file.size = fileInfos[i].size;
             strcpy(shared_data->largest_file.filename, fileInfos[i].filename);
-            pthread_mutex_unlock(&shared_data->mutex);
         }
 
         if (!shared_data->smallest_file_set || fileInfos[i].size < shared_data->smallest_file.size) {
-            pthread_mutex_lock(&shared_data->mutex);
             shared_data->smallest_file.size = fileInfos[i].size;
             strcpy(shared_data->smallest_file.filename, fileInfos[i].filename);
             shared_data->smallest_file_set = 1;
-            pthread_mutex_unlock(&shared_data->mutex);
         }
+        pthread_mutex_unlock(&shared_data->mutex);
     }
 }
 void firstDepth(const char *path, struct data *shared_data) {
@@ -148,12 +160,18 @@ void firstDepth(const char *path, struct data *shared_data) {
                 waitpid(childProcessID, NULL, 0);
             }
         }
-        // Check if it's a regular file
+            // Check if it's a regular file
         else if (stat(file_path, &file_stat) == 0 && S_ISREG(file_stat.st_mode)) {
             // Update total number of files
             // Lock the mutex before accessing shared data
             pthread_mutex_lock(&shared_data->mutex);
-
+            if (shared_data->total_files < MAX_FILES) {
+                // Copy file info to the array
+                FileInfo temp;
+                temp.size = file_stat.st_size;
+                strcpy(temp.filename, file_path);
+                memcpy(&shared_data->file_infos[shared_data->total_files], &temp, sizeof(FileInfo));
+            }
             // Update total number of files in the shared data
             shared_data->total_files++;
             shared_data->final_size += file_stat.st_size;
@@ -200,11 +218,12 @@ int main(int argc, char *argv[]){
     printf("Largest file: %s, Size: %ld bytes\n", shared_data->largest_file.filename, shared_data->largest_file.size);
     printf("Smallest file: %s, Size: %ld bytes\n", shared_data->smallest_file.filename, shared_data->smallest_file.size);
     printf("Final size of the root folder: %ld bytes\n", shared_data->final_size);
+    for (int i = 0; i < shared_data->total_files; ++i) {
+        printf("file %d: size: %ld bytes path: %s\n", i+1, shared_data->file_infos[i].size, shared_data->file_infos[i].filename);
+    }
     // Cleanup: close shared memory and unlink
     munmap(shared_data, sizeof(struct data));
     close(shared_memory_fd);
     shm_unlink("/shared_data");
     pthread_mutex_destroy(&shared_data->mutex);
-
-
 }
